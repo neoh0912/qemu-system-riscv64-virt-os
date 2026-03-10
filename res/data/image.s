412765f995e513b39809116b0fb185675963ccb7
neoh0912 <neoh@marsta.net>
2026-03-10 09:44:48 +0100

    update

--- res/data/image.s
+++ res/data/image.s
@@ -1,3 +1,7 @@
-image: .incbin "image.bmp"
+image: .fill 16,4,0xFFFFFFFF
+.rep 14
+.fill 1,4,0xFFFFFFFF; .fill 14,4,0x00000000; .fill 1,4,0xFFFFFFFF
+.endr
+.fill 16,4,0xFFFFFFFF
 image_w: .byte 1
 image_h: .byte 1
