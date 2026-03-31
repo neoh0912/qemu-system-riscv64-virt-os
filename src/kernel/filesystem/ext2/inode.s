8cf364b2294e09f2c572be1449993ea6f865ddb8
neoh0912 <neoh@marsta.net>
2026-03-31 12:15:07 +0200

    u ext2

--- src/kernel/filesystem/ext2/inode.s
+++ src/kernel/filesystem/ext2/inode.s
@@ -1,7 +1,18 @@
+.include "const/fs/mount.s"
+.include "const/ext2.s"
 ext2_get_inode:
 #[ci [ mount, index ]
-        save an=2
+        save an=2,sn=1
 
+        ld t0,fs_mount__superblock_data(a0)
+        lwu t1,s_inodes_per_group(t0)
+
+        addi t2,a1,-0x1
+        divu t1,t2,t1
+        addi s1,t1,0x2
+
+        
+        
         
 
         restore
